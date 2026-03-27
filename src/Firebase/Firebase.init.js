8c00bc209a9ffecc491e0c6a2a732f9a9721b9da
MD.TASNEM <tasnimmuhammad51@gmail.com>
2026-03-27 14:31:21 +0600

    Integrate authentication  page

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDU9gNMdWTvhNm8pw7wTqGK9_liHNVGFdA",
-  authDomain: "warmpaws-pet-care-5b70e.firebaseapp.com",
-  projectId: "warmpaws-pet-care-5b70e",
-  storageBucket: "warmpaws-pet-care-5b70e.firebasestorage.app",
-  messagingSenderId: "491885801517",
-  appId: "1:491885801517:web:2a149e141729f1de8e48a4"
+  apiKey: "AIzaSyAH0PkQ5_7_43kb71ItxAS7XEomb_fdr78",
+  authDomain: "ecotrack-1837c.firebaseapp.com",
+  projectId: "ecotrack-1837c",
+  storageBucket: "ecotrack-1837c.firebasestorage.app",
+  messagingSenderId: "417652671860",
+  appId: "1:417652671860:web:76e5f41758ca67bdf09821",
 };
 
 // Initialize Firebase
