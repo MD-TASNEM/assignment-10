58af1059bf98a33c64979460ccbacd357e05a90c
MD.TASNEM <tasnimmuhammad51@gmail.com>
2026-04-01 11:19:48 +0600

    remove all unuse files

--- EcoTrack-Client/src/Pages/Home.jsx
+++ EcoTrack-Client/src/Pages/Home.jsx
@@ -744,7 +744,7 @@ const Home = () => {
               Join local events and meet fellow eco-enthusiasts
             </p>
           </div>
-          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
             {loading
               ? [1, 2, 3, 4].map((i) => <SkeletonCard key={i} />)
               : events.slice(0, 3).map((event, idx) => (
